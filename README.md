2421c90c019f2b9617ca89b7f36d3405d61dc906
Kacper Marcinkiewicz <marcinkiewicz.kacper@gmail.com>
2023-12-23 17:00:48 +0100

    Separate recordings and create readme

--- README.md
+++ README.md
@@ -1,3 +1,54 @@
-# RTSP Recorder - Node.JS
+# RTSP Video Recorder - Node.JS
 
-Simple recording to files
+This script is designed to record a video stream from an RTSP source and save the recordings to the specified directory. It uses the `fluent-ffmpeg` library to handle the RTSP stream and create video files in MKV format.
+
+## Features
+
+- **Continuous Recording:** The script continuously records the video stream and starts a new recording when the previous one ends.
+
+- **File Naming:** Recordings are named based on the start time, with the filename format `hour-minute-second.mkv`.
+
+- **Automatic Deletion:** Monitors the available disk space, and if it exceeds the defined threshold, it deletes the oldest recordings to free up space.
+
+## Configuration
+
+To use the script, create a `config.json` file with the following parameters (based on `config.json.example` file):
+
+- **rtspUrl:** The RTSP stream URL.
+- **recordingsDirectory:** The base directory where recordings are saved.
+- **segmentTimeSeconds:** The duration of each recording segment in seconds.
+- **diskSpaceThresholdMb:** The maximum size of the recording directory in megabytes. After the directory exceeds this size, the script will delete the oldest recordings to free up space.
+
+Example `config.json`:
+
+```json
+{
+    "rtspUrl": "rtsp://admin:passwd@192.168.1.12/h264/main/av_stream",
+    "recordingsDirectory": "recordings",
+    "diskSpaceThresholdMb": 10000,
+    "segmentTimeSeconds": 60
+}
+```
+
+## Running the Script
+
+To run the script, first install the dependencies:
+
+```bash
+npm install
+```
+---
+Then run the script:
+```bash
+npm start
+```
+
+or
+
+```bash
+node index.js
+```
+
+## Stopping the Script
+
+To stop the script, press `Ctrl + C` in the terminal window where it is running.
--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const ffmpeg = require('fluent-ffmpeg');
 const fs = require('fs-extra');
 const path = require('path');
 
-
 const configPath = 'config.json';
 if (!fs.existsSync(configPath)) {
     console.error("No config file ", configPath, " found");
@@ -13,21 +12,29 @@ try {
     const configContent = fs.readFileSync(configPath, 'utf8');
     config = JSON.parse(configContent);
 } catch (error) {
-    console.error('Error wile reading config:', error);
+    console.error('Error while reading config:', error);
 }
 
 const rtspUrl = config.rtspUrl;
-const recordingsDirectory = config.recordingsDirectory;
+const baseRecordingsDirectory = config.recordingsDirectory;
 const segmentTime = config.segmentTimeSeconds || 120;
 
-// create recordings directory if not exists
-fs.ensureDirSync(recordingsDirectory);
-
-let currentRecording = null;
+// start recording
+console.log('Starting recording.');
+startRecording();
+monitorDiskSpace();
 
 function startRecording() {
-    const timestamp = new Date().toISOString().replace(/:/g, '-');
-    const outputPath = path.join(recordingsDirectory, `recording_${timestamp}.mkv`);
+    const startTimestamp = new Date();
+    const dateFolder = startTimestamp.toISOString().split('T')[0].replace(/-/g, '.');
+    const timeFilename = startTimestamp.toISOString().split('T')[1].split('.')[0].replace(/:/g, '-');
+
+    const recordingsDirectory = path.join(baseRecordingsDirectory, dateFolder);
+
+    // create recordings directory if not exists
+    fs.ensureDirSync(recordingsDirectory);
+
+    const outputPath = path.join(recordingsDirectory, `${timeFilename}.mkv`);
 
     currentRecording = ffmpeg(rtspUrl)
         .inputFormat('rtsp')
@@ -50,30 +57,113 @@ function startRecording() {
                 startRecording();
             }, 50);
         });
-        currentRecording.run();
+
+    // stop recording after x seconds
+    setTimeout(() => {
+        console.log('Stopping segment..');
+        stopRecording();
+    }, segmentTime * 1000);
+
+    currentRecording.run();
 }
 
 function stopRecording() {
-    return new Promise((resolve) => {
-        if (currentRecording) {
-            currentRecording.on('end', () => {
-                currentRecording = null;
-                resolve();
-            });
-            currentRecording.ffmpegProc.stdin.write('q');
-            currentRecording.ffmpegProc.stdin.end();
-        } else {
-            resolve();
-        }
-    });
+    if (currentRecording) {
+        currentRecording.ffmpegProc.stdin.write('q');
+        currentRecording.ffmpegProc.stdin.end();
+        console.log('Recording stopped.');
+    }
 }
 
-// start recording
-console.log('Starting recording.');
-startRecording();
+function monitorDiskSpace() {
+    const interval = 5 * 1000;
+    setInterval(async () => {
+        const spaceThresholdMB = config.diskSpaceThresholdMb; // max size of folder in MB
 
-// stop recording after x seconds
-setInterval(() => {
-    console.log('Stopping segment..');
-    stopRecording();
-}, config.segmentTimeSeconds * 1000);
+        try {
+            const files = await fs.promises.readdir(config.recordingsDirectory);
+            const folderSizeMB = await calculateFolderSize(files, config.recordingsDirectory) / (1024 * 1024); // to MB
+
+            console.log(`Folder size: ${folderSizeMB}MB`);
+            if (folderSizeMB > spaceThresholdMB) {
+                console.log(`Folder size exceeds ${spaceThresholdMB}MB. Deleting oldest files.`);
+                await removeOldestFile(config.recordingsDirectory);
+            }
+        } catch (error) {
+            console.error('Error while reading folder size ', error);
+        }
+    }, interval);
+}
+
+
+async function calculateFolderSize(files, folderPath) {
+    let size = 0;
+
+    for (const file of files) {
+        const filePath = path.join(folderPath, file);
+        const stats = await fs.promises.stat(filePath);
+
+        if (stats.isDirectory()) {
+            const subFiles = await fs.promises.readdir(filePath);
+            size += await calculateFolderSize(subFiles, filePath);
+        } else {
+            size += stats.size;
+        }
+    }
+
+    return size;
+}
+
+async function removeOldestFile(baseDirectory) {
+    try {
+      const folders = await fs.readdir(baseDirectory);
+  
+      if (folders.length > 0) {
+        const oldestFolder = folders.reduce((oldest, folder) => {
+          const folderPath = path.join(baseDirectory, folder);
+          const stats = fs.statSync(folderPath);
+  
+          if (stats.isDirectory()) {
+            return stats.birthtimeMs < oldest.stats.birthtimeMs ? { folder, stats } : oldest;
+          } else {
+            return oldest;
+          }
+        }, { folder: null, stats: { birthtimeMs: Infinity } });
+  
+        if (oldestFolder.folder) {
+          const folderPath = path.join(baseDirectory, oldestFolder.folder);
+          const filesInFolder = await fs.readdir(folderPath);
+  
+          if (filesInFolder.length > 0) {
+            const oldestFile = filesInFolder.reduce((oldest, file) => {
+              const filePath = path.join(folderPath, file);
+              const stats = fs.statSync(filePath);
+              return stats.birthtimeMs < oldest.stats.birthtimeMs ? { file, stats } : oldest;
+            }, { file: null, stats: { birthtimeMs: Infinity } });
+  
+            if (oldestFile.file) {
+              await fs.unlink(path.join(folderPath, oldestFile.file));
+              console.log('Oldest file in oldest folder was deleted: ', oldestFile.file);
+  
+              // check if folder is empty
+              const isFolderEmpty = (await fs.readdir(folderPath)).length === 0;
+              if (isFolderEmpty) {
+                await fs.rmdir(folderPath);
+                console.log('Oldest folder was empty so it was deleted', oldestFolder.folder);
+              }
+            } else {
+              console.log('No files to delete in oldest folder');
+            }
+          } else {
+            console.log('Oldest folder is empty, no files to delete');
+          }
+        } else {
+          console.log('No folders to delete');
+        }
+      } else {
+        console.log('No folders to delete');
+      }
+    } catch (err) {
+      console.error('Error reading dictionary ', err);
+    }
+  }
